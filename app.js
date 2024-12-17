414885298365bd1120204f9cebe2a230eafbbb54
Margarita Vychik <aeternam.hiberna@gmail.com>
2024-12-17 03:57:21 +0300

    fix(no_rule): add checkbox className

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className = "checkbox";
     editInput.type="text";
     editInput.className="input";
 
