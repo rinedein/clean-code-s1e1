7a16447cc50eaa851c0d39ae6a1a2613462a5f36
Margarita Vychik <aeternam.hiberna@gmail.com>
2024-12-17 03:25:14 +0300

    fix(no_rule): delete double addTask

--- app.js
+++ app.js
@@ -150,7 +150,7 @@ var ajaxRequest=function(){
 
 
 //Set the click handler to the addTask function.
-addButton.onclick=addTask;
+// addButton.onclick=addTask;
 addButton.addEventListener("click",addTask);
 addButton.addEventListener("click",ajaxRequest);
 
