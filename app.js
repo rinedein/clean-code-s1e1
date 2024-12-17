f723939e5d76fe516b1027a1b0eff1e53dd83b35
Margarita Vychik <aeternam.hiberna@gmail.com>
2024-12-17 03:06:41 +0300

    refactor(basic-3.4/extended-2.1): del tag selectors and add BEM

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    deleteButtonImg.className="button__icon";
+    listItem.className="list__item";
     label.innerText=taskString;
     label.className='label';
 
